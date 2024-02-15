57c3282c88c03ea415655473b08e1fb6deb1456b
William <wgatson@ufl.edu>
2024-02-15 05:42:48 -0500

    First1

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 var form = document.getElementById('add-todo');
 var input = document.getElementById('todo-input');
 var list = document.getElementById('todo-list');
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+    var text = input.value;
+    var item = document.createElement('li');
+    item.textContent = text;
+    list.appendChild(item);
+    input.value = '';
+    }
+    addEventListener(textValue);
+    form.reset();
+    }
+    function addEventListener(textValue) {
+        var item = document.createElement('li');
+        item.textContent = textValue;
+        list.appendChild(item);
+    }
+
+    list.addEventListener('click', function(event) {
+      if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('done');
+        if (event.target.classList.contains('done')) {
+          event.target.classList.remove('done');
+        }
+        }
